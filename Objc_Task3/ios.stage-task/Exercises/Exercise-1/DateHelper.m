1eaf7e69385e2b12213e889a50eef8d30e202023
anton <antonmiklis@icloud.com>
2021-05-23 23:42:01 +0300

    feat: add task 1 task 2

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,24 +5,48 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
-    return nil;
+    NSDateFormatter *formatter= [[NSDateFormatter alloc] init];
+    formatter.dateFormat = @"MM";
+    NSDate *date = [formatter dateFromString:[NSString stringWithFormat:@"%lu", monthNumber]];
+    NSDateFormatter *formatterOut= [[NSDateFormatter alloc] init];
+    formatterOut.dateFormat = @"MMMM";
+    return [formatterOut stringFromDate:date];
 }
 
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+    NSDateFormatter *formatter= [[NSDateFormatter alloc] init];
+    formatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ss'Z'";
+    NSDate *d = [formatter dateFromString:date];
+    NSDateFormatter *formatterOut= [[NSDateFormatter alloc] init];
+    formatterOut.dateFormat = @"dd";
+    return [[formatterOut stringFromDate:d] integerValue];
 }
 
 #pragma mark - Third
 
 - (NSString *)getDayName:(NSDate*) date {
-    return nil;
+    NSDateFormatter *formatterOut= [[NSDateFormatter alloc] init];
+    NSLocale *locale = [[NSLocale alloc]
+                         initWithLocaleIdentifier:@"ru"];
+    [formatterOut setLocale:locale];
+    formatterOut.dateFormat = @"EE";
+    return [formatterOut stringFromDate:date];
 }
 
 #pragma mark - Fourth
 
 - (BOOL)isDateInThisWeek:(NSDate *)date {
+    NSCalendar *gregorian = [[NSCalendar alloc]
+                             initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    NSDateComponents *weekdayComponents =
+                        [gregorian components:(NSCalendarUnitDay |
+                                               NSCalendarUnitYear | NSCalendarUnitMonth) fromDate:date];
+    if ([weekdayComponents day] <= 23 && [weekdayComponents day] >= 17 && [weekdayComponents year] == 2021 && [weekdayComponents month] == 5){
+        return YES;
+    }
+    
     return NO;
 }
 
--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -1,9 +1,35 @@
 #import "PlayersSeparator.h"
 
 @implementation PlayersSeparator
-
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    int possibleTeams = 0;
+    if ([ratingArray count] < 3) {
+        return 0;
+    }
+    for (int i = 0; i < [ratingArray count] - 2; i++) {
+        for (int j = i + 1; j < [ratingArray count] - 1; j++) {
+            if ([ratingArray[i] intValue] > [ratingArray[j] intValue]) {
+                for (int k = j + 1; k <[ratingArray count]; k++) {
+                    if ([ratingArray[j] intValue] > [ratingArray[k] intValue]) {
+                        possibleTeams++;
+                    }
+                }
+            }
+        }
+    }
+    ratingArray = [[ratingArray reverseObjectEnumerator] allObjects];
+    for (int i = 0; i < [ratingArray count] - 2; i++) {
+        for (int j = i + 1; j < [ratingArray count] - 1; j++) {
+            if ([ratingArray[i] intValue] > [ratingArray[j] intValue]) {
+                for (int k = j + 1; k <[ratingArray count]; k++) {
+                    if ([ratingArray[j] intValue] > [ratingArray[k] intValue]) {
+                        possibleTeams++;
+                    }
+                }
+            }
+        }
+    }
+    return possibleTeams;
 }
 
 @end
